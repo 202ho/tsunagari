d835f12c1a361cd498c11f21f76a4d09a51b8c8a
eymin1259 <eymin1259@gmail.com>
2024-09-11 10:02:56 +0900

    first commit

---
+++ src/test/java/com/tsunagari/TsunagariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsunagari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsunagariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
