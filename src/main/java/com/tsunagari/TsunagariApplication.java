291ed9af8336cbe61ed898d44ff1201a3e029d67
CoU-0 <dasz1233@naver.com>
2024-09-13 14:48:17 +0900

    test commit
    test chae

--- src/main/java/com/tsunagari/TsunagariApplication.java
+++ src/main/java/com/tsunagari/TsunagariApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TsunagariApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TsunagariApplication.class, args);
 	}
