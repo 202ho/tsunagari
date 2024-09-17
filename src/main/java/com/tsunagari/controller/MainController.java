0ba9f59f8af8032a9eecc8c233a28d58ef57c5cc
CoU-0 <dasz1233@naver.com>
2024-09-17 16:37:43 +0900

    add commit

---
+++ src/main/java/com/tsunagari/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.tsunagari.controller;
+
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+// 중요 키테고리 리스트
+
+
+
+
+// 액티비티 키워드 검색
+
+//인기 액티비티 조회
+
+}
