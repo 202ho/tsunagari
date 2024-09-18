4af88c1e434203bede8d8d562b7862f2312a0bcc
CoU-0 <dasz1233@naver.com>
2024-09-18 20:47:16 +0900

    add
    추가

--- src/main/java/com/tsunagari/controller/MainController.java
+++ src/main/java/com/tsunagari/controller/MainController.java
@@ -1,18 +1,29 @@
 package com.tsunagari.controller;
 
 
+import com.tsunagari.repository.ActivityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+/*
+* 1. 액티비티 키워드 검색
+* 2. 인기 액티비티 조회
+* 3. 주요 카테고리 리스트
+*/
 @Controller
 public class MainController {
 
-// 중요 키테고리 리스트
+    //액티비티 키워드 검색
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    @GetMapping("/main")
+    public String SearchKeyword(Model model){
 
 
-
-
-// 액티비티 키워드 검색
-
-//인기 액티비티 조회
+    return "ActivityList";
+    }
 
 }
