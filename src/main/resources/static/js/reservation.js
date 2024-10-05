b670e5be6e94b4b7919783386fc9c1dac0a3e42d
eymin1259 <eymin1259@gmail.com>
2024-10-05 18:06:30 +0900

    add : 호스트 예약관리 달력 ui

--- src/main/resources/static/js/reservation.js
+++ src/main/resources/static/js/reservation.js
@@ -1,6 +1,55 @@
 
 $(document).ready(function() {
 
+const Calendar2 = tui.Calendar;
+const calendar = new Calendar2('#calendar-app', {
+  defaultView: 'month',
+  useDetailPopup:true,
+  gridSelection: true,
+  isReadOnly: true,
+  calendars: [
+    {
+      id: 'cal1',
+      name: ''
+    }
+  ],
+    timezone: {
+      zones: [
+        {
+          timezoneName: 'Asia/Seoul',
+          displayLabel: 'UTC+9:00',
+          tooltip: 'Seoul'
+        }
+      ]
+      }
+});
+
+calendar.createEvents([
+{
+    id: '1',
+    calendarId: 'cal1',
+    title: 'my event',
+    category: 'allday',
+    location: "서울",
+    start: '2024-10-05T22:30:00+09:00',
+    end: '2024-10-05T22:30:00+09:00',
+    state:'',
+    attendees:['2']
+  },
+  {
+    id: '2',
+        calendarId: 'cal1',
+        title: 'my event',
+        category: 'allday',
+        location: "서울",
+    start: '2024-10-06T22:30:00+09:00',
+    end: '2024-10-06T22:30:00+09:00',
+        backgroundColor: '#f1caf9',
+
+  },
+
+]);
+
 });
 
 
--- src/main/webapp/WEB-INF/views/host/reservation.jsp
+++ src/main/webapp/WEB-INF/views/host/reservation.jsp
@@ -1,11 +1,16 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ include file="/WEB-INF/views/common/docHead.jsp" %>
+<link rel="stylesheet" href="https://uicdn.toast.com/calendar/latest/toastui-calendar.min.css" />
+<script src="https://uicdn.toast.com/calendar/latest/toastui-calendar.min.js"></script>
+<script src="/resources/js/reservation.js"></script>
+<link href="/resources/css/reservation.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
     <%@ include file="/WEB-INF/views/common/header.jsp" %>
-    <h2>호스트 예약 관리 페이지</h2>
-    https://ui.toast.com/tui-calendar
+    <section class="calendar-app-layout" style="display:flex; justify-content:center; align-items:center;">
+              <main id="calendar-app" style="width:800px; height:800px;"></main>
+    </section>
     <%@ include file="/WEB-INF/views/common/footer.jsp" %>
 </body>
 
