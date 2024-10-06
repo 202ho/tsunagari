14fb685a4241e6a9c2ee00db8e05d87d97db218d
eymin1259 <eymin1259@gmail.com>
2024-10-06 18:07:11 +0900

    add : 이미지 s3 업로드 기능 개발

--- src/main/java/com/tsunagari/activity/controller/ActivityApiController.java
+++ src/main/java/com/tsunagari/activity/controller/ActivityApiController.java
@@ -1,7 +1,11 @@
 package com.tsunagari.activity.controller;
 
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.tsunagari.activity.service.ActivityService;
+import com.tsunagari.s3.S3Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +19,9 @@ public class ActivityApiController {
 
     @Autowired
     private ActivityService activityService;
+
+    @Autowired
+    private S3Service s3service;
 
     @PostMapping("/new")
     public ResponseEntity<?> createActivity(
@@ -30,14 +37,18 @@ public class ActivityApiController {
             @RequestParam("new-activity-y") String latitude
     ) {
 
+        String s3url = s3service.uploadImageToS3(photo);
+
         System.out.println("");
         System.out.println("title => " + title);
+        System.out.println("s3url => " + s3url);
         System.out.println("content => " + content);
         System.out.println("address => " + address);
         System.out.println("address2 => " + address2);
         System.out.println("price => " + price);
         System.out.println("category => " + category);
         System.out.println("photo => " + photo.getOriginalFilename());
+
         System.out.println("latitude => " + latitude);
         System.out.println("longitude => " + longitude);
 
