a27015f7399f393c5ca191fc02e0645048097d0d
wjddnr2099 <wjddnr2099@naver.com>
2024-10-09 10:16:22 +0900

    post화면 작업
    post화면 작업

--- src/main/java/com/tsunagari/Post/controller/PostController.java
+++ src/main/java/com/tsunagari/Post/controller/PostController.java
@@ -1,4 +1,8 @@
 package com.tsunagari.Post.controller;
+
+import com.tsunagari.Post.entity.Post;
+import com.tsunagari.Post.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
